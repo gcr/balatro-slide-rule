246030fb1b7ce4e3b53ece934c9aa8fff0ccee27
Kimberly Wilber <kimmy@kjwilber.org>
2024-10-03 10:12:12 -0400

    Added readme, and made minor improvements (removing dips inside text borders etc)

--- make-slide-rule.py
+++ make-slide-rule.py
@@ -5,8 +5,88 @@ app = marimo.App(width="medium")
 
 
 @app.cell
-def __(Path, np, plt):
-    def prep_ax(plot_heights=[95]):
+def __(mo):
+    mo.md(
+        r"""
+        # Rotary slide rule
+
+        ## [Usage video](https://www.youtube.com/watch?v=85sJeg3v7Jg), [Github](https://github.com/gcr/balatro-slide-rule)
+
+        - Rotary logarithmic scale with three octaves for multiplying numbers between 001 and 999. 
+          - I really like the 3-octave design because you can think in terms of "ones," "thousands," or "millions" depending on situation
+          - Calculation wraps around, eliminating "off-scale" calculation problems of linear slide rules
+        - This notebook saves inner.pdf and outer.pdf
+        - Open in Affinity Designer, expand strokes, make sure there's no self-intersecting geometry, and export to SVG
+        - Open the resulting files in Fusion 360. Use the center "X" to align.
+        """
+    )
+    return
+
+
+@app.cell
+def __(annotate, mo, np, out, plt, prep_ax, start):
+    # Polar coordinates: (theta, r)
+    ax = prep_ax([start])
+
+
+    # X in center, useful for aligning inner and outer in Fusion 360
+    ax.plot([0,np.pi], [10,10])
+    ax.plot([np.pi*0.5,np.pi*1.5], [10,10])
+
+    # Plot has three octaves
+    scales = [1, 10, 100]
+
+    # Where to place notches? (theta)
+    longnotches =    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    shortnotches =   [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
+    shorternotches = [x+a for a in [1,2,3,4] for x in [.1, .2, .3, .4, .6, .7, .8, .9]]
+
+    # Line lengths (r)
+    llength = 5 if out.value else -5
+    longline =       [start, start+llength]
+    shortline =      [start+llength*0.5, start+llength]
+    shorterline =    [start+llength*0.5, start+llength*0.4]
+
+    # Text
+    fontsize = 10
+    prefix="" # change to "x " to add multiplier in front of all digits
+    xytext=(0.01, start+llength*3.5)
+
+    path="scale-out.pdf" if out.value else "scale-in.pdf"
+
+    annotate(ax, scales, longnotches,
+            line=longline,
+            textformat=prefix+"%s", lw=1.7, xytext=xytext, size=fontsize)
+    annotate(ax, scales, shortnotches,
+             line=shortline)
+    annotate(ax, scales, shorternotches,
+             line=shorterline)
+    annotate(ax, 1,        [1.5, 2.5], textformat=prefix+"%.1f", xytext=xytext, size=fontsize)
+    annotate(ax, [10,100], [1.5, 2.5], textformat=prefix+"%d", xytext=xytext, size=fontsize)
+    plt.savefig(path)
+    mo.vstack([mo.md("# Slide rule preview"), out, ax])
+    return (
+        ax,
+        fontsize,
+        llength,
+        longline,
+        longnotches,
+        path,
+        prefix,
+        scales,
+        shorterline,
+        shorternotches,
+        shortline,
+        shortnotches,
+        xytext,
+    )
+
+
+@app.cell
+def __(np, plt):
+    start = 95
+    def prep_ax(ring_heights=[95]):
+        """Start making a new plot, in polar coordinates"""
         ax = plt.subplot(111, polar=True)
 
         # Make the labels go clockwise
@@ -20,12 +100,15 @@ def __(Path, np, plt):
         # * draw our own axis circle
         # * draw our own tick marks (below)
         ax.set_ylim(0,130)
-        #ax.grid(False)
+        ax.grid(False)
         ax.set_frame_on(False)
         ax.axes.get_yaxis().set_visible(False)
 
         angles = np.linspace(0.0, 2.0*np.pi, 100)
-        ax.plot( angles, 100*plot_heights, color='black', lw=0.2 )
+        # plot a single ring
+        if ring_heights is not None:
+            ax.plot( angles, 100*ring_heights, color='black', lw=0.2 )
+        # all ticks are made manually via plt.annotate()
         ax.set(xticks=[], yticks=[])
         return ax
 
@@ -51,66 +134,11 @@ def __(Path, np, plt):
                         rotation=-t*180/np.pi + 90,
                         ha='center',
                         va='center',
-                        font=Path("/Users/kimmy/Downloads/"
-                                  "m6x11plus.ttf"),
+    #                    font=Path("/Users/kimmy/Downloads/
+    #                              "m6x11plus.ttf"),
                         size=size,
                     )
-    return annotate, prep_ax
-
-
-@app.cell
-def __(mo):
-    out = mo.ui.switch(False, label="Out facing")
-    out
-    return (out,)
-
-
-@app.cell
-def __(out):
-    start = 95
-    prefix=""
-    llength = 5 if out.value else -5
-    path="scale-out.pdf" if out.value else "scale-in.pdf"
-    return llength, path, prefix, start
-
-
-@app.cell
-def __(annotate, llength, np, path, plt, prefix, prep_ax, start):
-    scales = [1, 10, 100]
-    ax = prep_ax([start])
-    ax.plot([0,np.pi], [10,10])
-    ax.plot([np.pi*0.5,np.pi*1.5], [10,10])
-    longnotches =    [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    longline =       [start, start+llength]
-    shortline =      [start+llength*0.5, start+llength]
-    shortnotches =   [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
-    shorternotches = [x+a for a in [1,2,3,4] for x in [.1, .2, .3, .4, .6, .7, .8, .9]]
-    shorterline =    [start+llength*0.5, start+llength*0.4]
-    xytext=(0.01, start+llength*2.5)
-    size=10
-    annotate(ax, scales, longnotches,
-            line=longline,
-            textformat=prefix+"%s", lw=1.7, xytext=xytext, size=size)
-    annotate(ax, scales, shortnotches,
-             line=shortline)
-    annotate(ax, scales, shorternotches,
-             line=shorterline)
-    annotate(ax, 1,        [1.5, 2.5], textformat=prefix+"%.1f", xytext=xytext, size=size)
-    annotate(ax, [10,100], [1.5, 2.5], textformat=prefix+"%d", xytext=xytext, size=size)
-    plt.savefig(path)
-    ax
-    return (
-        ax,
-        longline,
-        longnotches,
-        scales,
-        shorterline,
-        shorternotches,
-        shortline,
-        shortnotches,
-        size,
-        xytext,
-    )
+    return annotate, prep_ax, start
 
 
 @app.cell
@@ -120,8 +148,15 @@ def __():
     import numpy as np
     from pathlib import Path
 
-    # from https://stackoverflow.com/questions/69080109/matplotlib-making-polar-plot-with-logarithmic-angular-axis
+    # adapted from https://stackoverflow.com/questions/69080109/matplotlib-making-polar-plot-with-logarithmic-angular-axis
     return Path, mo, np, plt
+
+
+@app.cell
+def __(mo):
+    # UI switches
+    out = mo.ui.switch(True, label="Text is outside ring")
+    return (out,)
 
 
 if __name__ == "__main__":
